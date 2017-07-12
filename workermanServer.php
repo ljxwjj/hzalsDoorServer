d6c1c73b9e5f835c2004f6ad9100677693703d62
ljxwjj <ljxwjj@163.com>
2017-07-12 18:10:25 +0800

    优化测试程序

--- workermanServer.php
+++ workermanServer.php
@@ -19,7 +19,7 @@ $ws_worker->count = 4;
 $ws_worker->onMessage = function($connection, $data)
 {
     $date = date("Ymd");
-    $logfile = dirname(__FILE__).DIRECTORY_SEPARATOR . "$date udp.log";
+    $logfile = dirname(__FILE__).DIRECTORY_SEPARATOR ."log".DIRECTORY_SEPARATOR. "$date udp.log";
     file_put_contents($logfile, $data."\n", FILE_APPEND);
     exec("php door/udp.php /Index/index/data/$data", $info);
     file_put_contents($logfile, $info[0]."\n", FILE_APPEND);
