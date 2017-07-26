dd4dfcc15d83b047d50ba989de09507a9791f527
ljxwjj <ljxwjj@163.com>
2017-07-26 15:08:53 +0800

    qrcode

--- ApplicationApi/Controller/UcenterController.class.php
+++ ApplicationApi/Controller/UcenterController.class.php
@@ -45,22 +45,22 @@ class UcenterController extends CommonRestController {
         $user = $User->find(I('user_id'));
 
         if (!$user) {
-            $result = createResult(1, '非系统用户');
+            $result = $this->createResult(1, '非系统用户');
         } else if (empty($user['password'])) {
-            $result = createResult(2, '帐号未激活');
+            $result = $this->createResult(2, '帐号未激活');
         } else if ($user['status'] === -1) {
-            $result = createResult(3, '用户被禁用');
+            $result = $this->createResult(3, '用户被禁用');
         } else if ($user['password'] !== $password){
-            $result = createResult(0, '原密码错误');
+            $result = $this->createResult(0, '原密码错误');
         } else {
             $data['password'] = $new_password;
             $data['update_time'] = time();
             $saveFlag = $User->save($data);
 
             if ($saveFlag) {
-                $result = createResult(200, '找回成功');
+                $result = $this->createResult(200, '找回成功');
             } else {
-                $result = createResult(0, '找回失败');
+                $result = $this->createResult(0, '找回失败');
             }
         }
 
