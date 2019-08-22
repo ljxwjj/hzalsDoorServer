9983f0426084bcad6664304eed7bb94e13eb8f55
ljxwjj <ljxwjj@163.com>
2019-08-22 18:34:51 +0800

    fix bug；

--- ApplicationApi/Controller/UfaceManagerController.class.php
+++ ApplicationApi/Controller/UfaceManagerController.class.php
@@ -45,9 +45,8 @@ class UfaceManagerController extends RestController {
 
 //                $currentTime = time();
 //                if ($showTime > $currentTime - 10000 && $showTime < $currentTime + 10000) {
-                    $doorController = A("DoorController");
                     if ($data['product_type'] == 1) {
-                        $rv = $doorController->sendOpenDoorHttp($data['ip'], $data['port'], $door_id, $data['password']);
+                        $rv = $this->sendOpenDoorHttp($data['ip'], $data['port'], $door_id, $data['password']);
                         if ($rv) {
                             $OpenRecord->where("id=$addid")->setField("feedback_time",time());
                             $result = $this->createResult(201, "开门成功");
@@ -56,7 +55,7 @@ class UfaceManagerController extends RestController {
                         }
                     } else {
                         $wait = intval($data['wait_time']);
-                        $doorController->sendOpenDoorUdpCode($data['ip'], $data['port'], $data['serial_number'], $door_id, $wait);
+                        $this->sendOpenDoorUdpCode($data['ip'], $data['port'], $data['serial_number'], $door_id, $wait);
                         $result = $this->createResult(200, "开门成功", array("id"=>$addid));
                     }
 //                } else {
@@ -85,6 +84,45 @@ class UfaceManagerController extends RestController {
         $result['message'] = $message;
         $result['data'] = $data;
         return $result;
+    }
+
+
+    protected function sendOpenDoorUdpCode($ip, $port, $serialNumber, $doorId, $wait) {
+        $handle = stream_socket_client("udp://127.0.0.1:9998", $errno, $errstr);
+        if( !$handle ){
+            die("ERROR: {$errno} - {$errstr}\n");
+        }
+        $sendMsg = "30030001"; // 开门指令
+        $ips = explode(".", $ip);
+        foreach ($ips as $i) {
+            $sendMsg .= sprintf("%02x", $i);
+        }
+        $sendMsg .= sprintf("%04x", $port);
+        $sendMsg .= "01";
+        $sendMsg .= $serialNumber;
+        $sendMsg .= sprintf("%02x", $doorId);
+        $sendMsg .= sprintf("%04x", $wait);
+        $sendMsg = hex2bin($sendMsg);
+        fwrite($handle, $sendMsg);
+        fclose($handle);
+    }
+
+    protected function sendOpenDoorHttp($ip, $port, $doorId, $password) {
+        $opts = array(
+            'http'=>array('method'=>'GET', 'timeout'=>5)
+        );
+        //创建数据流上下文
+        $context = stream_context_create($opts);
+        if (empty($password)) {
+            $url = "http://$ip:$port/t.cgi?T,access_io,door,$doorId,1";
+        } else {
+            $url = "http://$ip:$port/t.cgi?T$password,access_io,door,$doorId,1";
+        }
+        $html =file_get_contents($url, false, $context);
+        if (strcasecmp($html, "ok") === 0) {
+            return true;
+        }
+        return false;
     }
 }
 
